08bcc85f76c8bb1882e497f556b3325b30a74115
aceyzz <data-xvi@protonmail.com>
2024-04-27 11:01:27 +0200

    refactor main()

--- src/main/main.c
+++ src/main/main.c
@@ -6,7 +6,7 @@
 /*   By: cedmulle <42-xvi@protonmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 08:59:51 by cedmulle          #+#    #+#             */
-/*   Updated: 2024/04/27 09:40:33 by cedmulle         ###   ########.fr       */
+/*   Updated: 2024/04/27 11:01:15 by cedmulle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,13 @@ int	main(int argc, char **argv)
 	// check_args(argc, argv);// Chris -> check si argument c'est bon
 	// parse_data(&data, argv[1]);// Chris -> parse les données + checks
 	// init_mlx(&data);// ced
-	// launch_game(&data);// ced
+	// init_textures(&data);// ced
+	// init_raycaster(&data);// ced
+	// mlx_hook(data.mlx->win, 2, 1L << 0, key_press, &data);// ced
+	// mlx_hook(data.mlx->win, 3, 1L << 1, key_release, &data);// ced
+	// mlx_hook(data.mlx->win, 17, 1L << 17, exit_game, &data);// ced
+	// mlx_loop_hook(data.mlx->mlx, game_loop, &data);// ced
+	// mlx_loop(data.mlx->mlx);// ced
+	// exit_game(&data);// ced
 	return (0);
 }
